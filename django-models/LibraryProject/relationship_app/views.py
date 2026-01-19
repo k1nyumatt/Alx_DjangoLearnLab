6a3350c7c4104e6c09750ac3b4f0998dff21c5de
martin kinyua <kinyuam664@gmail.com>
2026-01-19 09:50:47 +0300

    fixing views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from django.views.generic.detail import DetailView
 from .models import Book, Library, Author
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 
 
 # Function-based view to list all books
