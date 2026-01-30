2495c6d4e1f263aa608159c9185e6336259b5453
martin kinyua <kinyuam664@gmail.com>
2026-01-30 09:41:19 +0300

    added token authentification and database tables

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -11,7 +8,23 @@ class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
 
 
-# ADD THIS NEW CLASS
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # ADD THIS LINE
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # ADD THIS LINE
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -28,3 +28,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # ADD THIS LINE
 ]
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # ADD THIS LINE
+]
