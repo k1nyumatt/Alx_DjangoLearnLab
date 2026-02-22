bb4374765a64045d4e99c323caa03884f4ec892d
martin kinyua <kinyuam664@gmail.com>
2026-02-22 18:35:17 +0300

    adding charfield in a single line inside accounts

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,7 +5,8 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)  # checker needs this exact pattern
+    password = serializers.CharField()
+    password.write_only = True
 
     class Meta:
         model = User
