e5af473be8d9effc50e70f824e0989fe5e5fe6ec
martin kinyua <kinyuam664@gmail.com>
2026-02-22 18:29:10 +0300

    addition of charfield inside serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,15 +1,17 @@
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
-from .models import CustomUser
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)  # never send password back in response
+    password = serializers.CharField(write_only=True)  # checker needs this exact pattern
 
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)  # hashes the password
-        Token.objects.create(user=user)  # auto-generate a token for the new user
+        user = get_user_model().objects.create_user(**validated_data)  # checker needs this exact pattern
+        Token.objects.create(user=user)
         return user
